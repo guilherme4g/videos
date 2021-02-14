cc19581412645ba30096eaa5373f4d5abfdc5ac9
jose guilherme <jgnvs6@gmail.com>
2021-02-14 00:42:26 -0300

    feat: create VideoDetail component

--- src/components/VideoDetail/VideoDetail.tsx
+++ src/components/VideoDetail/VideoDetail.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const VideoDetail: React.FC = () => <div> Video Detail </div>;
+interface IPropsVideoDetail {
+  video: any;
+}
+
+const VideoDetail: React.FC<IPropsVideoDetail> = ({ video }) => {
+  if (!video.id) {
+    return <div> Waiting </div>;
+  }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe title={video.snippet.title} src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
+};
+
+VideoDetail.propTypes = {
+  video: propTypes.objectOf(() => {
+    return null;
+  }).isRequired,
+};
 
 export default VideoDetail;
